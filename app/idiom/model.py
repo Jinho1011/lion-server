ccfb1e15a9481c22bfab3760b24c32dc33b833bb
Jeon <gino9940@gmail.com>
2023-03-27 11:37:35 +0900

    fix: Idiom relation

--- app/idiom/model.py
+++ app/idiom/model.py
@@ -4,9 +4,9 @@ from app.db.base import Base
 
 
 class Idiom(Base):
-    __tablename__ = "idiom"
+    __tablename__ = "Idiom"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    hanja = Column(String, nullable=False, unique=True)  # 故事成語
+    hanja = Column(String, unique=True)  # 故事成語
     hangeul = Column(Text, nullable=False)  # 고사성어
     meaning = Column(String, nullable=False)  # 고사에서 유래된 한자어 관용어
