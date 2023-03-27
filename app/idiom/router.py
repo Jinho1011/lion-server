2523327a08058c22e0d8bd91e3a2a3a37cf20578
Jeon <gino9940@gmail.com>
2023-03-27 11:37:54 +0900

    feat: add delete endpoint

--- app/idiom/router.py
+++ app/idiom/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.database import get_session
@@ -15,6 +15,14 @@ async def get_idioms(db: AsyncSession = Depends(get_session)):
     return idioms.scalars().fetchall()
 
 
+@idiom_router.get('/init')
+async def init_matcher(db: AsyncSession = Depends(get_session)):
+    idioms = await db.execute(select(Idiom))
+    result = idioms.scalars().all()
+    from app.main import matcher
+    matcher.init(result)
+
+
 @idiom_router.post("/")
 async def create_idioms(req: IdiomCreate, db: AsyncSession = Depends(get_session)):
     idiom = Idiom(**req.dict())
@@ -23,6 +31,13 @@ async def create_idioms(req: IdiomCreate, db: AsyncSession = Depends(get_session
 
 
 @idiom_router.get("/search/{meaning}")
-async def search_idioms(meaning, db: AsyncSession = Depends(get_session)):
-    idioms = await db.execute(select(Idiom))
-    return idioms.scalars().fetchall()
+async def search_idioms(meaning):
+    from app.main import matcher
+    return matcher.find(meaning)
+
+
+@idiom_router.delete("/{id}")
+async def delete_idiom(id, db: AsyncSession = Depends(get_session)):
+    idiom = await db.execute(delete(Idiom).where(Idiom.id == int(id)))
+    await db.commit()
+    return idiom
